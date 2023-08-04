fa7668eabdb26c04ed8340927e3d3db282c7b4f4
Victor Cardoso <98980071+vek03@users.noreply.github.com>
2023-08-04 16:15:03 -0300

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,6 +82,14 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('client.register')" :active="request()->routeIs('client.register')">
+                {{ __('Cadastrar Cliente') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('client.list')" :active="request()->routeIs('client.list')">
+                {{ __('Listar Clientes') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
